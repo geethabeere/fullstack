5f2367b5886e371c3b53a3b07b1de393adeb29aa
GEETHU <beeregeethasree@gmail.com>
2019-10-02 16:15:55 +0530

    second

---
+++ add.js
@@ -0,0 +1,17 @@
+var str="geetha";
+var str1='gee';
+var str2=`
+<h1>hai</h1>
+<p>irtw0tuwritiwtiwrtwptwrptowpjttutt</p>
+<input type="text" placeholder="enter the string">
+<button>submit</button>
+${2+2}
+
+`;
+document.write( str + str1 + str2 );
+
+var name="anu";
+var age=20;
+var company="infosys";
+var salary=20000;
+ console.log(`my name is <strong>${} `);
