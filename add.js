c33d28add380397ce2e9d84897922775935b0c17
GEETHU <beeregeethasree@gmail.com>
2019-10-03 23:36:33 +0530

    arrays

--- add.js
+++ add.js
@@ -14,4 +14,52 @@ var name="anu";
 var age=20;
 var company="infosys";
 var salary=20000;
- console.log(`my name is <strong>${} `);
+ console.log(`my name is ${name} i am ${age} old i worked for ${company} and i earned ${salary} for month`);
+
+ var name ="anu";
+ var name1 =" ggeyadf";
+ console.log (typeof name);
+ console.log (typeof name1);
+
+ //number
+ var num =12;
+ var number2=23;
+ var num=56;
+ console.log(typeof num);
+ console.log(typeof number);
+  console.log(1 / 0 ); 
+
+
+  var x=10;
+  var y=20;
+  var z=x+y;
+  document.write(z);
+  
+  var x=10, age ="30",name="abcd";
+  document.write(x+"<br>");
+  document.write(age+ "<br>");
+  document.write(name);
+
+
+
+  //non primitive datatypes and object reff....
+  let languages =['java'  , 'nodejs' , 'python' , 'rudy' , 'null'];
+  console.log(languages[0]); 
+
+
+
+  var str ='heelo i am geetha sreee';
+   
+var strwitharray = str.split('  ');
+console.log(strwitharray);
+
+let [...spread]=str;
+console.log(spread);
+
+
+let[...geetha] = str;
+console.log(geetha.toString(''));
+
+var esixway =array.from(str);
+console.log(esixway);
+
---
+++ arryconcatr.js
@@ -0,0 +1,9 @@
+let array2=['java' , 'pythan','express'];
+let array3=['sql' , 'angular', 'nodejs'];
+ let arraycancat=array2.concat(array3);
+ document.write(arraycancat);
+  
+
+ var str='geetha';
+ var a=[...str].reverse().join(' ');
+ document.write(a);
